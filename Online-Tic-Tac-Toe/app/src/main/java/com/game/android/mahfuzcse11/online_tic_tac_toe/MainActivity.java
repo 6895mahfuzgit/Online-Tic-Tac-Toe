c8c4011a122afcdc5e66497e20fd7bcb3c2e0ee9
6895mahfuz@gmail.com <6895mahfuz@gmail.com>
2017-06-28 02:03:40 +0600

    Added Game GUI

--- Online-Tic-Tac-Toe/app/src/main/java/com/game/android/mahfuzcse11/online_tic_tac_toe/MainActivity.java
+++ Online-Tic-Tac-Toe/app/src/main/java/com/game/android/mahfuzcse11/online_tic_tac_toe/MainActivity.java
@@ -2,12 +2,41 @@ package com.game.android.mahfuzcse11.online_tic_tac_toe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText etInviteEmal, etMyEmail;
+    Button buLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etInviteEmal = (EditText) findViewById(R.id.etInviteEmal);
+        etMyEmail = (EditText) findViewById(R.id.etMyEmail);
+        buLogin = (Button) findViewById(R.id.buLogin);
+    }
+
+    public void BuInvite(View view) {
+
+        Log.d("Invite", etInviteEmal.getText().toString());
+    }
+
+    public void BuAccept(View view) {
+
+        Log.d("Accept", etInviteEmal.getText().toString());
+    }
+
+    public void BuLogin(View view) {
+        Log.d("Log in", etMyEmail.getText().toString());
+    }
+
+    public void BuClick(View view) {
     }
 }
